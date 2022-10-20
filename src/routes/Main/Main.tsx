66ef02f4b9f3bf2a1c757d8eec55d1d81936ae8f
anna-shakhova <vasilyevaanna@gmail.com>
2022-10-20 16:48:29 +0200

    Filter items by title

--- src/routes/Main/Main.tsx
+++ src/routes/Main/Main.tsx
@@ -1,21 +1,49 @@
-import React, { FC, useCallback } from 'react';
-import { Fab } from '@mui/material';
+import React, { ChangeEvent, FC, useCallback, useEffect, useState } from 'react';
+import { Fab, FormControl, InputAdornment, InputLabel, OutlinedInput } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
+import SearchIcon from '@mui/icons-material/Search';
+import { debounce } from 'lodash';
 
 import { AssetCard, EditAssetDialog } from '../../components';
 import styles from './Main.module.scss';
 import { useAppDispatch, useAppSelector } from '../../store/Store';
 import { UserRole } from '../../store/Auth/Auth.types';
 import { openEditDialog } from '../../store/Assets/Assets.slice';
+import { IAssetItem } from '../../store/Assets/Assets.types';
 
 const Main: FC = () => {
   const dispatch = useAppDispatch();
   const { assets } = useAppSelector((state) => state.assets);
   const { role } = useAppSelector((store) => store.auth);
 
+  const [filter, setFilter] = useState('');
+  const [filteredAssets, setfilteredAssets] = useState<IAssetItem[]>(assets);
+
   const isAdmin = role === UserRole.Admin;
 
   const onAddHandler = useCallback(() => dispatch(openEditDialog()), [dispatch]);
+
+  const debounceFilterAssets = useCallback(
+    debounce(
+      (value: string) =>
+        setfilteredAssets(value
+          ? assets.filter((asset) => asset.title.toLowerCase().includes(value.toLowerCase()))
+          : assets),
+      300
+    ), [assets]
+  );
+
+  const onChangeFilterHandler = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setFilter(e.target.value);
+      debounceFilterAssets(e.target.value);
+    },
+    [debounceFilterAssets]
+  );
+
+  useEffect(() => {
+    !filter && setfilteredAssets(assets);
+  }, [assets, filter]);
 
   return (
     <div className={styles.root}>
@@ -26,8 +54,27 @@ const Main: FC = () => {
         </Fab>
       )}
 
+      <div className={styles.filters}>
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="filter">Filter</InputLabel>
+          <OutlinedInput
+            fullWidth
+            id="filter"
+            value={filter}
+            onChange={onChangeFilterHandler}
+            margin="dense"
+            endAdornment={(
+              <InputAdornment position="end">
+                <SearchIcon />
+              </InputAdornment>
+            )}
+            label="Filter"
+          />
+        </FormControl>
+      </div>
+
       <div className={styles.assets}>
-        {assets.map((asset) => <AssetCard key={asset.id} asset={asset} />)}
+        {filteredAssets.map((asset) => <AssetCard key={asset.id} asset={asset} />)}
       </div>
 
       <EditAssetDialog />
